297b294a555584b9d58d12ceaecca4f95145c8d9
emersonsemidio <emersons.emidio01@gmail.com>
2022-02-07 13:58:17 -0300

    Adição transaction

--- scrips.js
+++ scrips.js
@@ -15,25 +15,25 @@ let Modal = {
 
 const transactions = [
   {
-  id: 1,
+
   description: 'Luz',
   amount: -50000,
   date: '23/05/2021'
 },
 {
-  id: 2,
+
   description: 'Internet',
   amount: -20000,
   date: '23/05/2021'
 },
 {
-  id: 3,
+
   description: 'Novas Mobilias',
   amount: -15000,
   date: '23/05/2021'
 },
 {
-  id: 4,
+
   description: 'Yo Salario',
   amount: 150000,
   date: '20/05/2021'
@@ -41,6 +41,16 @@ const transactions = [
 ]
 
 const Transaction = {
+  all: transactions,
+
+  add(value){
+    Transaction.all.push(value)
+  },
+
+  remove(index){
+    Transaction.all.splice(index)
+  },
+
   incomes(){
     let income = 0
 
@@ -68,8 +78,6 @@ const Transaction = {
     return Transaction.incomes() + Transaction.expenses()
   }
 }
-
-
 
 
 const DOM = {
@@ -100,26 +108,40 @@ const DOM = {
 
   updateBalance(){
     document.getElementById('incomeDisplay')
-    .innerHTML = Transaction.incomes()
+    .innerHTML = Utils.formatCurrency( Transaction.incomes())
 
     document.getElementById('expenseDisplay')
-    .innerHTML = Transaction.expenses()
+    .innerHTML = Utils.formatCurrency(Transaction.expenses())
 
     document.getElementById('totalDisplay')
-    .innerHTML = Transaction.total()
+    .innerHTML = Utils.formatCurrency(Transaction.total())
   }
 }
 
 
 
 const Utils = {
+  formatAmount(value){
+    value = Number(value) * 100
+    return value
+  },
+
+  formatDate(date){
+    console.log(date)
+    const splittedDate = date.split("-")
+    return `${splittedDate[2]}/${splittedDate[1]}/${splittedDate[0]}`
+  },
+
   formatCurrency(value){
     const signal = Number(value) < 0 ? "-": "";
 
-
-    value = String(value).replace(/\D/, '')
+    
+    value = String(value).replace(/\D/, '').replace(',', '.')
+    console.log(value);
 
     value = Number(value)/100;
+    console.log(value);
+
 
 
     value = value.toLocaleString(`pt-BR`, {
@@ -129,12 +151,111 @@ const Utils = {
 
     return `${signal} ${value}`;
 
+  }
+
+
+}
+
+const Form = {
+  description: document.querySelector('input#description'),
+  amount: document.querySelector('input#amount'),
+  date: document.querySelector('input#date'),
+
+  getValues(){
+    return {
+      description: Form.description.value,
+      amount: Form.amount.value,
+      date: Form.date.value
+    }    
+  },
+
+  formatData(){
+
+  },
+
+  validateFields(){
+    console.log(Form.getValues());
+    const { description, amount, date} = Form.getValues();
+
+    if(description.trim() === "" || amount.trim() === "" || date.trim() === ""){
+      throw new Error('Por favor, preencha todos os campos')
+    }
+  },
+
+  formatValues(){
+    let { description, amount, date} = Form.getValues()
+
+    amount = Utils.formatAmount(amount)    
+
+    date = Utils.formatDate(date)
+
+    return{
+      description,
+      amount,
+      date
+    }
+
+  },
+
+  clearFields(){
+    Form.description.value = ""
+    Form.amount.value = ""
+    Form.date.value = ""
+  },
+
+
+  submit(event){
+    event.preventDefault()
+
+    try {
+      Form.validateFields()
+      const transaction = Form.formatValues()
+      Transaction.add(transaction)
+      DOM.addTransaction(transaction, null);
+      DOM.updateBalance()
+      Form.clearFields()
+      Modal.close()
+      // App.reload()
+
+    } catch (error) {
+      alert(error.message)
+      
+    }
 
   }
 }
 
-transactions.forEach(function(transaction){
-  DOM.addTransaction(transaction)
-})
+const Storage = {
+  get(){
+    return JSON.parse(localStorage.getItem("dev.finances:transactions")) || []
+  },
 
-DOM.updateBalance()
+  set(transaction){
+    localStorage.setItem("dev.finances:transactions", JSON.stringify(transaction))
+  }
+
+}
+
+
+
+DOM.updateBalance();
+
+
+
+const App = {
+  init(){
+    Transaction.all.forEach(function(transaction){
+      DOM.addTransaction(transaction)
+    })
+
+    DOM.updateBalance()
+  },
+
+  reload(){
+    //DOM.clearTransaction()
+    App.init()
+  }
+}
+
+App.init();
+
